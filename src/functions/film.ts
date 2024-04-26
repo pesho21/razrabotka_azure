17f066074e99a6ab8576b31b7302d8637a512638
pesho21 <petar.m.gerov.2020@elsys-bg.org>
2024-04-26 13:55:08 +0300

    test

--- src/functions/film.ts
+++ src/functions/film.ts
@@ -1,53 +1,11 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
 
 export async function film(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     context.log(`Http function processed request for url "${request.url}"`);
 
-    try {
-        const requestBody: string = request.body ? request.body.toString() : '';
+    const name = request.query.get('name') || await request.text() || 'world';
 
-        const parsedBody = requestBody ? JSON.parse(requestBody) : {};
-
-        const { title, year, genre, description, director, actors } = parsedBody;
-
-        if (title && year && genre && description && director && actors) {
-            try {
-                const newMovie = await prisma.movie.create({
-                    data: {
-                        title: title,
-                        year: year,
-                        genre: genre,
-                        description: description,
-                        director: director,
-                        actors: actors
-                    }
-                });
-
-                return {
-                    status: 200,
-                    body: JSON.stringify(newMovie) 
-                };
-            } catch (error) {
-                return {
-                    status: 500,
-                    body: error.message
-                };
-            }
-        } else {
-            return {
-                status: 400,
-                body: "You must provide all required fields: title, year, genre, description, director, actors"
-            };
-        }
-    } catch (error) {
-        return {
-            status: 400,
-            body: "Invalid JSON payload"
-        };
-    }
+    return { body: `Hello, ${name}!` };
 };
 
 app.http('film', {
